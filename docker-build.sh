7b734538714c3fff38fe91e8c7628542ddf97768
Clickot <clickot3@gmail.com>
2022-11-25 09:35:48 +0100

    only checkout if ungoogled-chromium dir is empty

--- docker-build.sh
+++ docker-build.sh
@@ -7,10 +7,12 @@ GIT_REPO="ungoogled-chromium"
 DISTRO_RELEASE=${1:-'debian:bullseye'}
 #DISTRO_RELEASE=${1:-'ubuntu:jammy'}
 LLVM_VERSION=${2:-'15'}
+NODE_VERSION=${3:-'16'}
 
 DISTRO=$(echo ${DISTRO_RELEASE}| cut -d':' -f1)
 RELEASE=$(echo ${DISTRO_RELEASE}| cut -d':' -f2)
 [ $LLVM_VERSION == "16" ] || REPO_POSTFIX="-$LLVM_VERSION"
+
 
 #LLVM_FULL_VERSION=$(curl -s https://api.github.com/repos/llvm/llvm-project/releases/latest | jq .tag_name | sed 's/llvmorg\-//' | sed 's/\"//g') 
 echo -e "using DISTRO='$DISTRO' RELEASE='$RELEASE' LLVM_VERSION='$LLVM_VERSION'\n" 
@@ -20,13 +22,13 @@ IMAGE="chromium-builder-${RELEASE}:llvm-${LLVM_VERSION}"
 cd $BASE_DIR 
 
 if [ -z "$(docker images -q ${IMAGE})" ] ; then
-    echo -e "image '${IMAGE}' not found, building it first.\ndocker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} ."
-    (cd $BASE_DIR/docker && docker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} . )
+    echo -e "image '${IMAGE}' not found, building it first.\ndocker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} ."
+    (cd $BASE_DIR/docker && docker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} . )
 else
     echo -e "reusing existing image '${IMAGE}'...\n"
 fi
 
-git submodule update --init --recursive
+[ -n "$(ls -A ungoogled-chromium)" ] || git submodule update --init --recursive
 
 BUILD_START=$(date)
 echo "==============================================================="
