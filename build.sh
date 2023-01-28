781f13328a40cbda347326ba9871d99ba65f9871
Clickot <clickot3@gmail.com>
2023-01-28 15:09:04 +0100

    minor cleanup in build files

--- build.sh
+++ build.sh
@@ -1,56 +1,47 @@
 #!/bin/bash -eux
 
-# Simple build script for Portable Linux
-# NOTE: This script will delete all build intermediates every time it runs. Proceed with caution.
+apt-get update && apt-get -y upgrade
 
-apt-get update && apt-get upgrade
+# this script is an adaption of https://github.com/ungoogled-software/ungoogled-chromium-portablelinux/blob/master/build.sh
 
-_root_dir=$(dirname $(readlink -f $0))
-_download_cache="$_root_dir/target/download_cache"
-_src_dir="$_root_dir/target/src"
-_main_repo="$_root_dir/ungoogled-chromium"
+root_dir=$(dirname $(readlink -f $0))
+download_cache="${root_dir}/target/download_cache"
+src_dir="${root_dir}/target/src"
+main_repo="${root_dir}/ungoogled-chromium"
 
-rm -rf "$_src_dir" || true
-rm -f "$_root_dir/target/domsubcache.tar.gz" || true
-mkdir -p "$_src_dir/out/Default"
-mkdir -p "$_download_cache"
+rm -rf "${src_dir}" || true
+rm -f "${root_dir}/target/domsubcache.tar.gz" || true
+mkdir -p "${src_dir}/out/Default"
+mkdir -p "${download_cache}"
 
-"$_main_repo/utils/downloads.py" retrieve -i "$_main_repo/downloads.ini" -c "$_download_cache"
-"$_main_repo/utils/downloads.py" unpack -i "$_main_repo/downloads.ini" -c "$_download_cache" "$_src_dir"
-"$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
-"$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches"
-"$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" -c "$_root_dir/target/domsubcache.tar.gz" "$_src_dir"
-cat "$_main_repo/flags.gn" "$_root_dir/flags.gn" > "$_src_dir/out/Default/args.gn"
+"${main_repo}/utils/downloads.py" retrieve -i "${main_repo}/downloads.ini" -c "${download_cache}"
+"${main_repo}/utils/downloads.py" unpack -i "${main_repo}/downloads.ini" -c "${download_cache}" "${src_dir}"
+"${main_repo}/utils/prune_binaries.py" "${src_dir}" "${main_repo}/pruning.list"
+"${main_repo}/utils/patches.py" apply "${src_dir}" "${main_repo}/patches"
+"${main_repo}/utils/domain_substitution.py" apply -r "${main_repo}/domain_regex.list" -f "${main_repo}/domain_substitution.list" -c "${root_dir}/target/domsubcache.tar.gz" "${src_dir}"
+cat "${main_repo}/flags.gn" "${root_dir}/flags.gn" > "${src_dir}/out/Default/args.gn"
 
-# Set commands or paths to LLVM-provided tools outside the script via 'export ...'
-# or before these lines
 export LLVM_VERSION=${LLVM_VERSION:=15}
 export AR=${AR:=llvm-ar-${LLVM_VERSION}}
 export NM=${NM:=llvm-nm-${LLVM_VERSION}}
 export CC=${CC:=clang-${LLVM_VERSION}}
 export CXX=${CXX:=clang++-${LLVM_VERSION}}
 export LLVM_BIN=${LLVM_BIN:=/usr/lib/llvm-${LLVM_VERSION}/bin}
-# You may also set CFLAGS, CPPFLAGS, CXXFLAGS, and LDFLAGS
-# See build/toolchain/linux/unbundle/ in the Chromium source for more details.
-#
-# Hack to allow clang to find the default cfi_ignorelist.txt and LLVM tools
-# -B<prefix> defined here: https://clang.llvm.org/docs/ClangCommandLineReference.html
-_llvm_resource_dir=$("$CC" --print-resource-dir)
-export CXXFLAGS+="-resource-dir=${_llvm_resource_dir} -B${LLVM_BIN}"
-export CPPFLAGS+="-resource-dir=${_llvm_resource_dir} -B${LLVM_BIN}"
-export CFLAGS+="-resource-dir=${_llvm_resource_dir} -B${LLVM_BIN}"
 
-cd "$_src_dir"
+llvm_resource_dir=$("$CC" --print-resource-dir)
+export CXXFLAGS+="-resource-dir=${llvm_resource_dir} -B${LLVM_BIN}"
+export CPPFLAGS+="-resource-dir=${llvm_resource_dir} -B${LLVM_BIN}"
+export CFLAGS+="-resource-dir=${llvm_resource_dir} -B${LLVM_BIN}"
 
-## hack to bypass the check for a certain llvm package version (seems to be introduced in chromium 107.xx)
+cd "${src_dir}"
+
+# hack to bypass the check for a certain llvm package version (seems to be introduced in chromium 107.xx)
 sed -i 's/ReadStampFile(STAMP_FILE).partition\(.*\)\[0\]/PACKAGE_VERSION/' ./tools/clang/scripts/update.py
 
 ./tools/gn/bootstrap/bootstrap.py -o out/Default/gn --skip-generate-buildfiles
 ./out/Default/gn gen out/Default --fail-on-unused-args
 
 # instead if installing node as node_module, simply link to the node binary installed on OS 
-# (to get around typescript compile errors beacuse of String<->TrustedHtml assignements that appeared since 109.xx)
-#(cd "$_src_dir/third_party/node" && npm install node-linux-x64)
 mkdir -p third_party/node/linux/node-linux-x64/bin && ln -s /usr/bin/node third_party/node/linux/node-linux-x64/bin/
 
 ninja -C out/Default chrome chrome_sandbox chromedriver
--- docker-build.sh
+++ docker-build.sh
@@ -13,20 +13,12 @@ DISTRO=$(echo ${DISTRO_RELEASE}| cut -d':' -f1)
 RELEASE=$(echo ${DISTRO_RELEASE}| cut -d':' -f2)
 [ $LLVM_VERSION == "16" ] || REPO_POSTFIX="-$LLVM_VERSION"
 
-
-#LLVM_FULL_VERSION=$(curl -s https://api.github.com/repos/llvm/llvm-project/releases/latest | jq .tag_name | sed 's/llvmorg\-//' | sed 's/\"//g') 
-echo -e "using DISTRO='$DISTRO' RELEASE='$RELEASE' LLVM_VERSION='$LLVM_VERSION'\n" 
-
 IMAGE="chromium-builder-${RELEASE}:llvm-${LLVM_VERSION}"
 
 cd $BASE_DIR 
 
-#if [ -z "$(docker images -q ${IMAGE})" ] ; then
-    echo -e "image '${IMAGE}' not found, building it first.\ndocker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} ."
-    (cd $BASE_DIR/docker && docker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} . )
-#else
-#    echo -e "reusing existing image '${IMAGE}'...\n"
-#fi
+echo "docker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} ."
+(cd $BASE_DIR/docker && docker build -t ${IMAGE} --build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg LLVM_VERSION=${LLVM_VERSION} --build-arg REPO_POSTFIX=${REPO_POSTFIX} --build-arg NODE_VERSION=${NODE_VERSION} . )
 
 [ -n "$(ls -A ungoogled-chromium)" ] || git submodule update --init --recursive
 
